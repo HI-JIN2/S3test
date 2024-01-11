313df4ff52a7243a13873afa95353f8d2dd32f6a
yujin <jini1514@soongsil.ac.kr>
2024-01-11 11:58:58 +0900

    fix: 모브제 참고

--- src/main/java/com/eddy/s3test/AwsS3Controller.java
+++ src/main/java/com/eddy/s3test/AwsS3Controller.java
@@ -1,10 +1,12 @@
 package com.eddy.s3test;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,18 +16,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/file")
-public class AmazonS3Controller {
+public class AwsS3Controller {
 
     private final AwsS3Service awsS3Service;
 
-    @PostMapping("/uploadFile")
-    public ResponseEntity<List<String>> uploadFile(List<MultipartFile> multipartFiles) {
-        return ResponseEntity.ok(awsS3Service.uploadFile(multipartFiles));
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "이미지 업로드 하기", description = "작품에 사용 될 이미지를 S3에 업로드하는 기능입니다.")
+    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        return awsS3Service.saveFile(file);
     }
 
-    @DeleteMapping("/deleteFile")
-    public ResponseEntity<String> deleteFile(@RequestParam String fileName) {
-        awsS3Service.deleteFile(fileName);
-        return ResponseEntity.ok(fileName);
-    }
 }
--- src/main/java/com/eddy/s3test/S3testApplication.java
+++ src/main/java/com/eddy/s3test/S3testApplication.java
@@ -2,8 +2,10 @@ package com.eddy.s3test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class S3testApplication {
 
 	public static void main(String[] args) {
